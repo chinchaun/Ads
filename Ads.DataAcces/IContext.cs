cf12dd0735cb6b2253083d3c94311f27fe54d8ae
Jean Delannoy <jjdelannoy@hotmail.com>
2015-04-14 21:39:46 -0300

    Data Project Created and interfaces implemented

---
+++ Ads.DataAcces/IContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.DataAcces
+{
+    public interface IContext
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void SaveOrUpdate();
+        IQueryable<T> Query<T>() where T : class;
+    }
+}
