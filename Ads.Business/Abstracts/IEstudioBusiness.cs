76b477700e2c94cea11861c111a19848eb4b0439
Jean Delannoy <jjdelannoy@hotmail.com>
2015-08-07 15:59:36 -0300

    Added Estudio Class

---
+++ Ads.Business/Abstracts/IEstudioBusiness.cs
@@ -0,0 +1,10 @@
+﻿using Ads.Model;
+
+
+namespace Ads.Business.Abstracts
+{
+    public interface IEstudioBusiness
+    {
+        estudio GetById(int id);
+    }
+}
---
+++ Ads.Business/EstudioBusiness.cs
@@ -0,0 +1,16 @@
+﻿using Ads.Service.Contracts;
+using Ads.Model;
+using Ads.Business.Abstracts;
+
+namespace Ads.Business
+{
+    public class EstudioBusiness : BusinessBase<IEstudioService>, IEstudioBusiness
+    {
+        public EstudioBusiness(IEstudioService service) : base(service) { }
+
+        public estudio GetById(int id)
+        {
+            return base.ServiceInstance.GetById(id);
+        }
+    }
+}
